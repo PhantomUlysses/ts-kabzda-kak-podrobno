be44217b8f90cb5677fd3c0191052851a468fdb0
Nikita Stasiulevich <nikitastas1694@gmail.com>
2022-03-28 22:16:18 +0300

    function in useState

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,28 @@
+import React, {useMemo} from "react";
+import {useState} from "react";
+
+export default {
+    title: 'useState demo',
+}
+
+function generateData() {
+    console.log("generateData");
+    return 1;
+}
+
+export const Example1 = () => {
+    console.log("Example1");
+
+    // const initValue = useMemo(generateData, []);
+
+    const [counter, setCounter] = useState(generateData);
+
+    const changer = (state: number) => {
+        return state + 1;
+    }
+
+    return <>
+        <button onClick={() => setCounter(changer)}>+</button>
+        {counter}
+    </>
+}
